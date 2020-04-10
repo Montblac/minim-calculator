586cac0dc726c48961896a85fce4376f57ad6da8
Montblac <montblac95@gmail.com>
2020-04-09 23:31:18 -0700

    Divide updateCurrent and doCalculation to smaller functions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import "./App.css";
 
+/*
+TODO
+ - Create function that adds number buttons programmatically, but may need
+   changing of how the buttons are organized in separate divs
+ - Create function that adds operator buttons programmatically, but same issue
+   as number buttons
+
+REF
+  - number buttons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+  - operator buttons = ["/", "%", "+", "x", "-", "=", "AC", "+/-"];
+*/
+
 function App() {
   const [current, setCurrent] = useState("");
   const [previous, setPrevious] = useState("");
   const [operator, setOperator] = useState("");
 
-  //const num_buttons = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-  //const op_buttons = ["/", "%", "+", "x", "-", "=", "AC", "+/-"];
-
-  // TODO: Fix updateCurrent to only take value
   const updateCurrent = value => {
-    setCurrent(current + value);
+    setCurrent(value);
   };
   const updatePrevious = value => {
     setPrevious(value);
@@ -37,34 +45,42 @@ function App() {
       setCurrent(num > 0 ? -Math.abs(num) : Math.abs(num));
     }
   };
-  const doCalculation = () => {
+  const doCalculate = (operand1, operand2, operator) => {
+    let result;
+    switch (operator) {
+      case "+":
+        result = operand1 + operand2;
+        break;
+      case "-":
+        result = operand1 - operand2;
+        break;
+      case "x":
+        result = operand1 * operand2;
+        break;
+      case "/":
+        result = operand1 / operand2;
+        break;
+      default:
+        console.log("Warning: Improperly handled calculation");
+    }
+    return result;
+  };
+  const onNumber = num => {
+    updateCurrent(current + num);
+  };
+  const onEqual = () => {
     if (current && previous && operator) {
-      let result;
-      let prev_temp = parseFloat(previous);
-      let curr_temp = parseFloat(current);
-
       updatePrevious("");
-      switch (operator) {
-        case "+":
-          result = prev_temp + curr_temp;
-          break;
-        case "-":
-          result = prev_temp - curr_temp;
-          break;
-        case "x":
-          result = prev_temp * curr_temp;
-          break;
-        case "/":
-          result = prev_temp / curr_temp;
-          break;
-        default:
-          console.log("Warning: Improperly handled calculation");
-      }
-
+      let result = doCalculate(
+        parseFloat(current),
+        parseFloat(previous),
+        operator
+      );
       setCurrent(result);
     }
   };
-  const doPercent = () => {
+
+  const onPercent = () => {
     if (current && previous && operator) {
       let prev_temp = parseFloat(previous);
       let curr_temp = parseFloat(current);
@@ -90,7 +106,7 @@ function App() {
               {" "}
               +/-{" "}
             </button>
-            <button onClick={e => updateOperator(e.target.value)} value="%">
+            <button onClick={() => onPercent()} value="%">
               {" "}
               %{" "}
             </button>
@@ -101,15 +117,15 @@ function App() {
           </div>
 
           <div>
-            <button onClick={e => updateCurrent(e.target.value)} value="7">
+            <button onClick={e => onNumber(e.target.value)} value="7">
               {" "}
               7{" "}
             </button>
-            <button onClick={e => updateCurrent(e.target.value)} value="8">
+            <button onClick={e => onNumber(e.target.value)} value="8">
               {" "}
               8{" "}
             </button>
-            <button onClick={e => updateCurrent(e.target.value)} value="9">
+            <button onClick={e => onNumber(e.target.value)} value="9">
               {" "}
               9{" "}
             </button>
@@ -119,15 +135,15 @@ function App() {
             </button>
           </div>
           <div>
-            <button onClick={e => updateCurrent(e.target.value)} value="4">
+            <button onClick={e => onNumber(e.target.value)} value="4">
               {" "}
               4{" "}
             </button>
-            <button onClick={e => updateCurrent(e.target.value)} value="5">
+            <button onClick={e => onNumber(e.target.value)} value="5">
               {" "}
               5{" "}
             </button>
-            <button onClick={e => updateCurrent(e.target.value)} value="6">
+            <button onClick={e => onNumber(e.target.value)} value="6">
               {" "}
               6{" "}
             </button>
@@ -137,15 +153,15 @@ function App() {
             </button>
           </div>
           <div>
-            <button onClick={e => updateCurrent(e.target.value)} value="1">
+            <button onClick={e => onNumber(e.target.value)} value="1">
               {" "}
               1{" "}
             </button>
-            <button onClick={e => updateCurrent(e.target.value)} value="2">
+            <button onClick={e => onNumber(e.target.value)} value="2">
               {" "}
               2{" "}
             </button>
-            <button onClick={e => updateCurrent(e.target.value)} value="3">
+            <button onClick={e => onNumber(e.target.value)} value="3">
               {" "}
               3{" "}
             </button>
@@ -156,15 +172,19 @@ function App() {
           </div>
 
           <div>
-            <button onClick={e => updateCurrent(e.target.value)} value="0">
+            <button onClick={e => onNumber(e.target.value)} value="0">
               {" "}
               0{" "}
             </button>
-            <button onClick={e => updateCurrent(e.target.value)} value=".">
+            <button onClick={e => onNumber(e.target.value)} value=".">
               {" "}
               .{" "}
             </button>
-            <button onClick={() => doCalculation()} className="=" value="=">
+            <button
+              onClick={() => onEqual({ current, previous })}
+              className="equal"
+              value="="
+            >
               {" "}
               ={" "}
             </button>
